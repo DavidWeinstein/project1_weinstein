22d6a3a1fe9bf9d09c16813b508a7129db78ded1
David Weinstein <weinstein.dev@gmail.com>
2020-09-24 16:28:12 -0400

    added some comments

--- LinkedStackWeinstein.java
+++ LinkedStackWeinstein.java
@@ -22,6 +22,9 @@ public final class LinkedStackWeinstein<T> implements StackInterfaceWeinstein<T>
     private Node head; // reference to head node in stack
     private int numberOfEntries;
 
+    /**
+     * Default constructor, creates an empty stack where head points to null
+     */
     public LinkedStackWeinstein() {
         head = null;
     } // end default constructor
@@ -49,7 +52,7 @@ public final class LinkedStackWeinstein<T> implements StackInterfaceWeinstein<T>
         head = head.getNextNode();
         numberOfEntries--;
         return end;
-    }
+    } 
 
     /** Retrieves this stack’s top entry.
     @return The object at the top of the stack or null if stack is empty */
@@ -63,7 +66,7 @@ public final class LinkedStackWeinstein<T> implements StackInterfaceWeinstein<T>
 
     /** Retrieves the stacks second entry
      * @return The second object in the stack 
-     * @throws NullPointerException if stack has fewer than 2 entries 
+     * @return null if less than 2 entries in stack 
      */
     public T peek2() {
         if (numberOfEntries<2) {
@@ -78,24 +81,42 @@ public final class LinkedStackWeinstein<T> implements StackInterfaceWeinstein<T>
         numberOfEntries = 0;
     }
 
-
+    /** Class to represent the top most element, or data, in a stack, and a link to the next object */
     private class Node {
         private T data; // entry in stack
         private Node next; // link to next node
 
+        /**
+         * Default contructor, sets dataPortion as top entry and points to null for second entry
+         * @param dataPortion first entry in a stack
+         */
         private Node(T dataPortion) {
             this(dataPortion, null);
         } // end constructor
 
+        /**
+         * Constructor data is newEntry dataPortion, and next is reference to the nextNode
+         *  which was formally the first, or top, node
+         * @param dataPortion data which will be the new top entry in stack
+         * @param nextNode formally top entry now will be reference to next node
+         */
         private Node(T dataPortion, Node nextNode) {
             data = dataPortion;
             next = nextNode;
         } // end constructor
 
+        /**
+         * Gets the data 
+         * @return data which is top entry in stack
+         */
         private T getData() {
             return data;
         } // end getData
 
+        /**
+         * Gets the next node
+         * @return next which would be the previous node
+         */
         private Node getNextNode() {
             return next;
         } // end getNext
