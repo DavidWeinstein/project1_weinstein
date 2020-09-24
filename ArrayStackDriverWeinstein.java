971ebcd8b3807d56ca0ab2d874a637d283fa90f2
David Weinstein <weinstein.dev@gmail.com>
2020-09-24 16:13:01 -0400

    driver class to test completed 1st draft

--- ArrayStackDriverWeinstein.java
+++ ArrayStackDriverWeinstein.java
@@ -4,14 +4,64 @@
 public class ArrayStackDriverWeinstein {
 
     public static void main(String[] args) {
-        System.out.println("****Testing****");
-        ArrayStackWeinstein<String> arrayTest = new ArrayStackWeinstein<>();
-        arrayTest.push("dave");
-        arrayTest.push("joe");
-        arrayTest.push("krissy");
-        System.out.println(arrayTest.pop());
-        System.out.println(arrayTest.peek());
-        System.out.println(arrayTest.peek2());
-        System.out.println(arrayTest.isEmpty());
+
+        ArrayStackWeinstein<String> stackTest = new ArrayStackWeinstein<>();
+        System.out.println("****Testing creation of empty stack****");
+        System.out.println("isEmpty() returns: " + stackTest.isEmpty());
+        System.out.println();
+
+        System.out.println("****Adding names to stack to test push()****");
+        stackTest.push("Jim");
+        stackTest.push("Jess");
+        stackTest.push("Jill");
+        stackTest.push("Jane");
+        stackTest.push("Joe");
+        System.out.println("isEmpty() returns: " + stackTest.isEmpty());
+        System.out.println();
+
+        System.out.println("****Testing peek, peek2, and pop****");
+        System.out.println(stackTest.peek() + " is at the top of the stack.");
+        System.out.println(stackTest.peek2() + " is just beneath the top of the stack.");
+        System.out.println(stackTest.pop() + " is removed from the stack.");
+        System.out.println();
+
+        System.out.println(stackTest.peek() + " is at the top of the stack.");
+        System.out.println(stackTest.peek2() + " is just beneath the top of the stack.");
+        System.out.println(stackTest.pop() + " is removed from the stack.");
+        System.out.println();
+
+        System.out.println(stackTest.peek() + " is at the top of the stack.");
+        System.out.println(stackTest.peek2() + " is just beneath the top of the stack.");
+        System.out.println(stackTest.pop() + " is removed from the stack.");
+        System.out.println();
+
+        System.out.println(stackTest.peek() + " is at the top of the stack.");
+        System.out.println(stackTest.peek2() + " is just beneath the top of the stack.");
+        System.out.println(stackTest.pop() + " is removed from the stack.");
+        System.out.println();
+
+        System.out.println(stackTest.peek() + " is at the top of the stack.");
+        System.out.println(stackTest.peek2() + " is just beneath the top of the stack.");
+        System.out.println(stackTest.pop() + " is removed from the stack.");
+        System.out.println();
+
+        System.out.println("****The stack should be empty****");
+        System.out.println("isEmpty() returns: " + stackTest.isEmpty());
+        System.out.println();
+
+        System.out.println("****Adding names to stack to test clear****");
+        stackTest.push("Joe");
+        stackTest.push("Jess");
+        stackTest.push("Jim");
+        System.out.println("****Testing clear****");
+        stackTest.clear();
+        System.out.println("****The stack should be empty****");
+        System.out.println("isEmpty() returns: " + stackTest.isEmpty());
+        System.out.println();
+
+        System.out.println("stackTest.peek() returns: " + stackTest.peek());
+        System.out.println("stackTest.pop() returns: " + stackTest.pop());
+        System.out.println();
+
     }
 }
